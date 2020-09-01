7f4a8973e7d89d7884fd373ef957681a09b838f1
Rocky <rockyspiker@gmail.com>
2020-09-01 17:48:00 -0600

    ajax form implementation

--- components/form.js
+++ components/form.js
@@ -1,11 +1,49 @@
+import { useEffect } from 'react'
 import Chevron from './icons/chevron'
 
 const Form = ({ className, ...props }) => {
+  useEffect(() => {
+    let form = document.getElementById('contact-me')
+    let button = document.getElementById('contact-me-submit')
+    let status = document.getElementById('contact-me-status')
+
+    function success() {
+      form.reset()
+      status.innerHTML = '<p>The form was submitted!</p>'
+    }
+
+    function error() {
+      status.innerHTML = '<p>Oops! There was a problem.</p>'
+    }
+
+    form.addEventListener('submit', function (e) {
+      e.preventDefault()
+      let data = new FormData(form)
+      ajax(form.method, form.action, data, success, error)
+    })
+  }, [])
+
+  function ajax(method, url, data, success, error) {
+    let xhr = new XMLHttpRequest()
+    xhr.open(method, url)
+    xhr.setRequestHeader('Accept', 'application/json')
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType)
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType)
+      }
+    }
+    xhr.send(data)
+  }
+
   return (
     <div className="container">
       <div className={`form-container ${className}`} {...props}>
         <form
-          action="https://formspree.io/rockyspiker.contact@gmail.com"
+          action="https://formspree.io/mnqgkwzo"
+          id="contact-me"
           method="POST"
         >
           <div className="info">
@@ -17,11 +55,18 @@ const Form = ({ className, ...props }) => {
           </div>
           <TextBlock label="Message" name="message" required />
           <div className="submit-container">
-            <button className="button is-primary" type="submit">
+            <button
+              className="button is-primary"
+              id="contact-me-submit"
+              type="submit"
+            >
               Send
             </button>
           </div>
         </form>
+        <div className="container">
+          <p id="contact-me-status"></p>
+        </div>
       </div>
     </div>
   )
